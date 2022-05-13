ef4646bebdf8091c32f46dd03d95f342f905e7d4
hyunjin1012 <hyunjin1012@gmail.com>
2022-05-12 21:12:13 -0700

    resize

--- pages/webtoons/index.js
+++ pages/webtoons/index.js
@@ -1,13 +1,17 @@
 import { webtoonData } from "../../homeComps/Homedata";
 import ListItem from "../../homeComps/List/ListItem";
 
-
 export default function Webtoons() {
-  return (<div className="container mx-auto flex flex-col my-5 md:grid md:grid-cols-2 md:my-10 xl:grid-cols-4">
-{webtoonData
-  .sort((a,b) => b.id - a.id)
-  .sort((a, b) => a.minted - b.minted).map((item, idx) => (
- <ListItem key={item.id} item={item} />
-))}
-  </div>)
+  return (
+    <div className="container mx-auto">
+      <div className="grid mx-8 my-10 gap-x-5 gap-y-10 sm:grid-cols-2 sm:my-20 sm:gap-x-8 sm:gap-y-14 lg:grid-cols-3 xl:grid-cols-4">
+        {webtoonData
+          .sort((a, b) => b.id - a.id)
+          .sort((a, b) => a.minted - b.minted)
+          .map((item, idx) => (
+            <ListItem key={item.id} item={item} />
+          ))}
+      </div>
+    </div>
+  );
 }
