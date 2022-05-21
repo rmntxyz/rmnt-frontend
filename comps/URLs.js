ab6a948a64935d7169cba3cd0c5e1f1f6be7a001
hyunjin1012 <hyunjin1012@gmail.com>
2022-05-21 00:29:29 -0700

    set headers to apis

--- comps/URLs.js
+++ comps/URLs.js
@@ -1,5 +1,5 @@
-// const baseUrl = "https://rmnt-frontend-git-feature-detail-rmnt.vercel.app/";
-const baseUrl = "http://localhost:3000/"
+const baseUrl = "https://rmnt-frontend-git-feature-detail-rmnt.vercel.app/";
+// const baseUrl = "http://localhost:3000/"
 const timeUrl = `${baseUrl}api/time/`;
 const webtoonsUrl = `${baseUrl}api/webtoons/`;
 const topUrl = `${baseUrl}api/webtoons/topData/`;
