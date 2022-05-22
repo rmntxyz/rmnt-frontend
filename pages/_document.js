1346d46a919a5180a9ae52dcc870d956cc6dd9f0
doodoji <doodozik@gmail.com>
2022-05-22 10:00:04 +0900

    edit next.config.js
    optimizeFonts: false

--- pages/_document.js
+++ pages/_document.js
@@ -5,6 +5,8 @@ class MyDocument extends Document {
     return (
       <Html>
         <Head>
+          <link rel="preconnect" href="https://fonts.gstatic.com" />
+          <link rel="preconnect" href="https://fonts.googleapis.com" />
           <link
             rel="stylesheet"
             href="https://fonts.googleapis.com/css2?family=Sen:wght@400;700;800&display=block"
