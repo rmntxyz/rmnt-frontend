ef39fed0a06a18f5e5d048ff10778c8f2e161a8b
hyunjin1012 <hyunjin1012@gmail.com>
2022-10-10 11:44:33 -0700

    add key to list on  /webtoons page

--- pages/webtoons/index.js
+++ pages/webtoons/index.js
@@ -98,7 +98,7 @@ export default function Webtoons({ webtoons }) {
       <Seo title="Webtoons | Rarement" />
       <main className="grid mx-8 my-10 gap-x-5 gap-y-10 sm:grid-cols-2 sm:my-20 sm:gap-x-8 sm:gap-y-14 lg:grid-cols-3 xl:grid-cols-4">
         {webtoons.map((item) => (
-          <ListItem key={item.webtoon_id} item={item} />
+          <ListItem key={item.id} item={item} />
         ))}
       </main>
     </div>
