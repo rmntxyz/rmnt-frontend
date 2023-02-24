83ca14c4be9e8d2728f50b7769d186f68fe481e2
hyunjin1012 <hyunjin1012@gmail.com>
2023-02-23 20:56:58 -0800

    add touchstart event

--- utils/getLoggedIn.js
+++ utils/getLoggedIn.js
@@ -2,13 +2,15 @@ import { useEffect, useState } from "react";
 
 export default function getLoggedIn(web3auth) {
   const [loggedIn, setLoggedIn] = useState(web3auth?.provider ? true : false);
-  function handleMousemove() {
+  function handleChange() {
     setLoggedIn(web3auth?.provider ? true : false);
   }
   useEffect(() => {
-    window.addEventListener("mousemove", handleMousemove);
-    handleMousemove();
-    return () => window.removeEventListener("mousemove", handleMousemove);
+    // window.addEventListener("mousemove", handleChange);
+    window.addEventListener("mousemove", handleChange)
+    window.addEventListener("touchstart", handleChange)
+    handleChange();
+    return () => window.removeEventListener("mousemove", handleChange);
   });
   return loggedIn;
 }
