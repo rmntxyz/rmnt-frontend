2099522721e76846c1849f6c281617f24ad0731f
hyunjin1012 <hyunjin1012@gmail.com>
2023-03-01 11:05:26 -0800

    reedit episode image width/height

--- comps/webtoonEpisode/KorEngEpisode.js
+++ comps/webtoonEpisode/KorEngEpisode.js
@@ -1,23 +1,22 @@
 import Image from "next/image";
 
 export default function KorEngEpisode({ props }) {
-  const imageRatio = props.height/props.width;
   return (
     <div>
       <Image
         id="episode"
         alt="Rarement Webtoon Image"
         src={props.imageUrl}
-        width={768}
-        height={768 * imageRatio}
+        width={props.width}
+        height={props.height}
         layout="responsive"
         objectFit="cover"
         placeholder="blur"
         blurDataURL={props.imageUrl}
-        loading="lazy"
-        // priority={true}
+        // loading="lazy"
+        priority={true}
         // quality={100}
-        // unoptimized
+        unoptimized
       />
     </div>
   );
