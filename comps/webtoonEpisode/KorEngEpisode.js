4ed15e35bb9b051bc6d1025cba0dd6642b047fca
hyunjin1012 <hyunjin1012@gmail.com>
2023-03-05 14:03:19 -0800

    disable lazy loading

--- comps/webtoonEpisode/KorEngEpisode.js
+++ comps/webtoonEpisode/KorEngEpisode.js
@@ -1,28 +1,20 @@
 import Image from "next/image";
 
 export default function KorEngEpisode({ data }) {
-  let imageUrl;
-  let imageWidth;
-  let imageHeight;
-  let imageRatio;
   return (
     <div id="episode">
       {data.map((item) => {
-        imageUrl = item.attributes.url;
-        imageWidth = item.attributes.width;
-        imageHeight = item.attributes.height;
-        imageRatio = imageHeight / imageWidth;
         return (
           <Image
             alt="Rarement Webtoon Image"
-            src={imageUrl}
+            src={item.attributes.url}
             width={768}
-            height={768 * imageRatio}
+            height={768 * (item.attributes.height / item.attributes.width)}
             layout="responsive"
             objectFit="cover"
             placeholder="blur"
-            blurDataURL={imageUrl}
-            loading="lazy"
+            blurDataURL={item.attributes.url}
+            // loading="lazy"
             // unoptimized
           />
         );
