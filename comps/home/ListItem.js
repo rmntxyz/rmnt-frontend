acf4381baa2a09c67cdbc0846de02691a60dca09
hyunjin1012 <hyunjin1012@gmail.com>
2023-04-01 18:29:38 -0700

    edit list item

--- comps/home/ListItem.js
+++ comps/home/ListItem.js
@@ -46,69 +46,70 @@ export default function ListItem({ item, idx, rarementABI }) {
           alt="Rarement Webtoon Cover Image"
         />
       </a>
-      <div
-        className={`absolute h-fit bottom-0 z-10 items-center w-full grounded-bl-2xl rounded-br-2xl bg-black/50 ${
-          idx === 0 ? "p-4 gap-4" : "p-3 gap-3"
-        }`}
-        style={{ display: released ? "flex" : "none" }}
-      >
-        <div className={`${idx === 0 ? "w-[23%]" : "w-[32%]"}`}>
-          <PolyFrameImage
-            href={item.attributes.avatarGIF.data.attributes.url}
-            idx={item.attributes.avatarGIF.data.id}
-          />
-        </div>
+      {released ? (
         <div
-          className={`flex flex-col -translate-y-[5%] ${
-            idx === 0 ? "w-[75%] gap-3 text-lg" : "w-[68%] gap-2 text-sm"
+          className={`absolute flex h-fit bottom-0 z-10 items-center w-full grounded-bl-2xl rounded-br-2xl bg-black/50 ${
+            idx === 0 ? "p-4 gap-4" : "p-3 gap-3"
           }`}
         >
-          <div className="flex gap-1.5 items-center">
-            <div className="relative" style={{ height: idx === 0 ? 44 : 33 }}>
-              <Image
-                src={
-                  item.attributes.artist_id.data.attributes.profile_image.data
-                    .attributes.url
-                }
-                width={idx === 0 ? 33 : 22}
-                height={idx === 0 ? 33 : 22}
-                alt="Rarement Artist Profile Image"
-                className="rounded-full"
-              />
-              <div className="absolute bottom-0 w-full h-px bg-white/50 rounded-sm"></div>
-            </div>
-            <div
-              className="relative w-full"
-              style={{ height: idx === 0 ? 44 : 33 }}
-            >
-              <a
-                href={
-                  "/artists/" +
-                  item.attributes.artist_id.data.attributes.first_name
-                }
-                className="hover:underline"
-              >
-                <span>Created by </span>
-                <span className="font-bold truncate">
-                  {item.attributes.artist_id.data.attributes.first_name}
-                </span>
-              </a>
-              <div className="absolute bottom-0 w-full h-px bg-white/50 rounded-sm"></div>
-            </div>
+          <div className={`${idx === 0 ? "w-[23%]" : "w-[32%]"}`}>
+            <PolyFrameImage
+              href={item.attributes.avatarGIF.data.attributes.url}
+              idx={item.attributes.avatarGIF.data.id}
+            />
           </div>
-          {isLoading ? (
-            <div className="animate-pulse  bg-lightGray/50 rounded-full items-center w-1/2 h-7"></div>
-          ) : (
+          <div
+            className={`flex flex-col -translate-y-[5%] ${
+              idx === 0 ? "w-[75%] gap-3 text-lg" : "w-[68%] gap-2 text-sm"
+            }`}
+          >
             <div className="flex gap-1.5 items-center">
-              <span>Collected</span>
-              <div className="flex items-center font-bold">
-                <span>{totalSupply}</span>
-                <span>/{rarementInfo?.maxSupply}</span>
+              <div className="relative" style={{ height: idx === 0 ? 44 : 33 }}>
+                <Image
+                  src={
+                    item.attributes.artist_id.data.attributes.profile_image.data
+                      .attributes.url
+                  }
+                  width={idx === 0 ? 33 : 22}
+                  height={idx === 0 ? 33 : 22}
+                  alt="Rarement Artist Profile Image"
+                  className="rounded-full"
+                />
+                <div className="absolute bottom-0 w-full h-px bg-white/50 rounded-sm"></div>
+              </div>
+              <div
+                className="relative w-full"
+                style={{ height: idx === 0 ? 44 : 33 }}
+              >
+                <a
+                  href={
+                    "/artists/" +
+                    item.attributes.artist_id.data.attributes.first_name
+                  }
+                  className="hover:underline"
+                >
+                  <span>Created by </span>
+                  <span className="font-bold truncate">
+                    {item.attributes.artist_id.data.attributes.first_name}
+                  </span>
+                </a>
+                <div className="absolute bottom-0 w-full h-px bg-white/50 rounded-sm"></div>
               </div>
             </div>
-          )}
+            {isLoading ? (
+              <div className="animate-pulse  bg-lightGray/50 rounded-full items-center w-1/2 h-7"></div>
+            ) : (
+              <div className="flex gap-1.5 items-center">
+                <span>Collected</span>
+                <div className="flex items-center font-bold">
+                  <span>{totalSupply}</span>
+                  <span>/{rarementInfo?.maxSupply}</span>
+                </div>
+              </div>
+            )}
+          </div>
         </div>
-      </div>
+      ) : null}
       {released ? null : (
         <div className="absolute bg-navBg/50 w-full h-full top-0 z-10 flex items-center justify-center text-white/100 text-3xl font-bold">
           Coming soon
