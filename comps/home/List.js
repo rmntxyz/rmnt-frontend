58bbed437653584fc3fe2670b71f4aaccc9829ad
doodoji <doodozik@gmail.com>
2023-04-09 01:59:47 +0900

    change to not use 'useScreenSize'
    Using [Effect Hook] to calculate the size of a DOM or window object,
    it will causes the UI to rattle during initial load or hydration.
    So simplifying without using it.

--- comps/home/List.js
+++ comps/home/List.js
@@ -2,51 +2,20 @@ import useScreenSize from "../../utils/useScreenSize";
 import ListItem from "./ListItem";
 
 export default function List({ webtoons, rarementABI }) {
-  //Get screen size to determine the layout
-  const { screenWidth } = useScreenSize();
+  const firstItem = webtoons[0];
+  const restItems = webtoons.slice(1);
 
   return (
     <div>
-      {screenWidth >= 640 ? (
-        <div className="grid grid-cols-1 gap-3">
-          {webtoons.slice(0, 1).map((item, idx) => (
-            <ListItem
-              key={item.id}
-              item={item}
-              idx={idx}
-              rarementABI={rarementABI}
-            />
-          ))}
-          {webtoons.slice(1)?.length % 2 === 0 ? (
-            <div className="grid grid-cols-1 gap-3 sm:grid-cols-2">
-              {webtoons.slice(1).map((item, idx) => (
-                <ListItem
-                  key={item.id}
-                  item={item}
-                  idx={idx + 1}
-                  rarementABI={rarementABI}
-                />
-              ))}
-            </div>
-          ) : (
-            <div className="grid grid-cols-1 gap-3 sm:grid-cols-2">
-              {webtoons.slice(1).map((item, idx) => (
-                <ListItem
-                  key={item.id}
-                  item={item}
-                  idx={idx + 1}
-                  rarementABI={rarementABI}
-                />
-              ))}
-              <div className="bg-opaqueGray rounded-2xl flex items-center justify-center aspect-square text-white/100 text-3xl font-bold">
-                Coming soon
-              </div>
-            </div>
-          )}
-        </div>
-      ) : (
-        <div className="grid grid-cols-1 gap-3">
-          {webtoons.map((item, idx) => (
+      <div className="grid grid-cols-1 gap-3">
+        <ListItem
+          key={firstItem.id}
+          item={firstItem}
+          idx={0}
+          rarementABI={rarementABI}
+        />
+        <div className="grid grid-cols-1 gap-3 sm:grid-cols-2">
+          {restItems.map((item, idx) => (
             <ListItem
               key={item.id}
               item={item}
@@ -54,8 +23,13 @@ export default function List({ webtoons, rarementABI }) {
               rarementABI={rarementABI}
             />
           ))}
+          {restItems.length % 2 === 1 &&
+            <div className="bg-opaqueGray rounded-2xl hidden sm:flex items-center justify-center aspect-square text-white/100 text-3xl font-bold">
+              Coming soon
+            </div>
+          }
         </div>
-      )}
+      </div>
     </div>
   );
 }
