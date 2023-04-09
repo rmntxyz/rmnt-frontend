2f425e6838481d2d0eed082e1bca89984d8532b6
hyunjin1012 <hyunjin1012@gmail.com>
2023-04-09 14:36:14 -0700

    customize connect options

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,8 @@
 import Layout from "../comps/layout/Layout";
 import "../styles/globals.css";
 import "0xpass/styles.css";
-import { getDefaultWallets, darkTheme, PassProvider } from "0xpass";
-import { configureChains, WagmiConfig, createClient } from "wagmi";
+import { darkTheme, PassProvider, createClient } from "0xpass";
+import { configureChains, WagmiConfig } from "wagmi";
 import { polygon, polygonMumbai } from "wagmi/chains";
 import { alchemyProvider } from "wagmi/providers/alchemy";
 import { publicProvider } from "wagmi/providers/public";
@@ -10,7 +10,7 @@ import { ApolloProvider } from "@apollo/client";
 import client from "../apollo";
 
 // Configure chains & providers with the Alchemy provider
-const { chains, provider } = configureChains(
+const { chains } = configureChains(
   [polygon, polygonMumbai],
   [
     alchemyProvider({
@@ -20,25 +20,25 @@ const { chains, provider } = configureChains(
   ]
 );
 
-const { connectors } = getDefaultWallets({
-  appName: "rmnt-frontend",
-  chains,
-});
-const wagmiClient = createClient({
-  autoConnect: true,
-  connectors,
-  provider,
-});
+// const { connectors } = getDefaultWallets({
+//   appName: "rmnt-frontend",
+//   chains,
+// });
+// const wagmiClient = createClient({
+//   autoConnect: true,
+// });
 
-// const wagmiClient = createClient();
+const passClient = createClient();
 // const chains = getDefaultChains();
 
 function MyApp({ Component, pageProps }) {
   return (
     <ApolloProvider client={client}>
-      <WagmiConfig client={wagmiClient}>
+      <WagmiConfig client={passClient}>
         <PassProvider
+          wagmiClientConfig={{ autoConnect: true }}
           apiKey="fa73399c-1ccd-4b68-9634-10ea59b6931f"
+          enabledConnectors={["google", "metamask"]}
           theme={darkTheme({
             accentColor: "#70EFCF",
             accentColorForeground: "black",
