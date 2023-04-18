bd4cf52a6e52bd41f5f8133e01383efb8f351e44
hyunjin1012 <hyunjin1012@gmail.com>
2023-04-18 14:06:35 -0700

    fix safari bug by removing foreign object

--- utils/PolyFrameImage.js
+++ utils/PolyFrameImage.js
@@ -1,33 +1,11 @@
-import Image from "next/legacy/image";
-import { isVideo } from "./mediaType";
-
 export const PolyFrameImage = ({ href, idx }) => (
   <svg viewBox="-1 -1 183 183" fill="none" xmlns="http://www.w3.org/2000/svg">
-    <foreignObject
+    <image
       width="100%"
       height="100%"
       clipPath={"url(#clip" + idx + ")"}
-    >
-      {isVideo.includes(href.split(".").pop()) ? (
-        <video
-          width="100%"
-          height="100%"
-          src={href}
-          autoPlay="autoplay"
-          muted
-          loop
-        ></video>
-      ) : (
-        <Image
-          layout="fill"
-          objectFit="cover"
-          src={href}
-          alt="Rarement Image"
-          placeholder="blur"
-          blurDataURL={href}
-        />
-      )}
-    </foreignObject>
+      xlinkHref={href}
+    />
     <path
       id={"path" + idx}
       d="M168 0.5C174.351 0.5 179.5 5.64873 179.5 12V143.569C179.5 147.262 177.726 150.731 174.731 152.893L140.888 177.324C138.929 178.739 136.574 179.5 134.157 179.5H12C5.64874 179.5 0.5 174.351 0.5 168V12C0.5 5.64872 5.64873 0.5 12 0.5H168Z"
