9a20644774905c447d0ea3cb387bfe1d9a1ee065
hyunjin1012 <hyunjin1012@gmail.com>
2023-04-18 21:26:50 -0700

    streamline header component

--- comps/layout/Header.js
+++ comps/layout/Header.js
@@ -1,10 +1,10 @@
-import { faArrowLeft, faWallet } from "@fortawesome/free-solid-svg-icons";
+import { faArrowLeft } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { useRouter } from "next/router";
 import usePrevRoute from "../../utils/usePrevRoute";
 import { Beta, Logo } from "../../utils/svgs";
-import { ConnectButton } from "0xpass";
 import Link from "next/link";
+import Connect from "./Connect";
 
 export default function Header() {
   //Use router to determine whether to show the back button or not & whether to display the header or not
@@ -48,113 +48,9 @@ export default function Header() {
         >
           <Logo />
           <Beta />
-          {/* <div className="hidden md:block">
-            <Logo />
-          </div>
-          <div className="block md:hidden">
-            <MediumLogo />
-          </div> */}
         </Link>
       </div>
-      <ConnectButton.Custom>
-        {({
-          account,
-          chain,
-          openAccountModal,
-          openChainModal,
-          openConnectModal,
-          mounted,
-        }) => {
-          return (
-            <div
-              {...(!mounted && {
-                "aria-hidden": true,
-                style: {
-                  opacity: 0,
-                  pointerEvents: "none",
-                  userSelect: "none",
-                },
-              })}
-            >
-              {(() => {
-                if (!mounted || !account || !chain) {
-                  return (
-                    <button
-                      onClick={openConnectModal}
-                      type="button"
-                      aria-label="Connect Wallet"
-                      className="py-3"
-                    >
-                      <span className="invisible px-8 py-3 bg-mintGreen border-2 border-mintGreen text-navBg text-base leading-tight font-bold rounded-3xl hover:bg-navBg hover:text-white duration-200 md:visible ">
-                        Connect Wallet
-                      </span>
-                      <span className="absolute top-6 right-8 p-2 bg-mintGreen border-2 border-mintGreen text-navBg text-base leading-tight rounded-full hover:bg-navBg hover:text-white duration-200 md:invisible">
-                        <FontAwesomeIcon
-                          icon={faWallet}
-                          width="20px"
-                          height="32px"
-                        ></FontAwesomeIcon>
-                      </span>
-                    </button>
-                  );
-                }
-
-                if (chain.unsupported) {
-                  return (
-                    <button onClick={openChainModal} type="button">
-                      Wrong network
-                    </button>
-                  );
-                }
-
-                return (
-                  <div className="flex items-center gap-1 sm:gap-2">
-                    <button
-                      onClick={openChainModal}
-                      style={{ display: "flex", alignItems: "center" }}
-                      type="button"
-                    >
-                      {chain.hasIcon && (
-                        <div
-                          style={{
-                            background: chain.iconBackground,
-                            width: 28,
-                            height: 28,
-                            borderRadius: 999,
-                            overflow: "hidden",
-                            marginRight: 4,
-                          }}
-                        >
-                          {chain.iconUrl && (
-                            <img
-                              alt={chain.name ?? "Chain icon"}
-                              src={chain.iconUrl}
-                              style={{ width: 28, height: 28 }}
-                            />
-                          )}
-                        </div>
-                      )}
-                      {/* {chain.name} */}
-                    </button>
-                    <button
-                      onClick={openAccountModal}
-                      type="button"
-                      className="flex flex-col text-xs sm:flex-row sm:gap-1 sm:text-lg"
-                    >
-                      <span>{account.displayName}</span>
-                      <span>
-                        {account.displayBalance
-                          ? ` (${account.displayBalance})`
-                          : ""}
-                      </span>
-                    </button>
-                  </div>
-                );
-              })()}
-            </div>
-          );
-        }}
-      </ConnectButton.Custom>
+      <Connect />
     </nav>
   );
 }
