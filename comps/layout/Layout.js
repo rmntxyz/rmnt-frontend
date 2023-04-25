45e5f9dbe718426a8df903b3c3892592d9463bf7
hyunjin1012 <hyunjin1012@gmail.com>
2023-04-25 15:37:37 -0700

    remove NextNProgress

--- comps/layout/Layout.js
+++ comps/layout/Layout.js
@@ -1,11 +1,11 @@
 import Footer from "./Footer";
 import Header from "./Header";
-import NextNProgress from "nextjs-progressbar";
+// import NextNProgress from "nextjs-progressbar";
 
 export default function Layout({ children }) {
   return (
     <div className="bg-navBg">
-      <NextNProgress color="#70EFCF" options={{ showSpinner: false }} />
+      {/* <NextNProgress color="#70EFCF" options={{ showSpinner: false }} /> */}
       <Header />
       {children}
       <Footer />
