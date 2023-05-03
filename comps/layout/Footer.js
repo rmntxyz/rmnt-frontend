9fbc21229a65aeeb32ad629e3f610d3b7d744655
hyunjin1012 <hyunjin1012@gmail.com>
2023-05-02 19:39:16 -0700

    add artist application link

--- comps/layout/Footer.js
+++ comps/layout/Footer.js
@@ -48,37 +48,48 @@ export default function Footer() {
       }}
     >
       <Line />
-      <ul className="flex items-center gap-5 mt-14">
-        <li>
-          <a
-            href="/"
-            target="_blank"
-            rel="noopener noreferrer"
-            aria-label="Follow Rarement on Instagram"
-            className="w-[44px] h-[44px] flex items-center justify-center duration-200 border border-white/20 rounded-full bg-opaqueGray hover:text-[#F3F3F3]"
-          >
-            <Instagram />
-          </a>
-        </li>
-        {OutLinks.map((link, idx) => (
-          <li
-            key={idx}
-            className="w-[44px] h-[44px] flex items-center justify-center duration-200 border border-white/20 rounded-full bg-opaqueGray hover:text-[#F3F3F3]"
-          >
-            {link}
+      <Link
+        href="https://forms.gle/Aboi8WTLm3214KNB7"
+        target="_blank"
+        rel="noopener noreferrer"
+        aria-label="Apply to be Rarement's artist"
+        className="text-mintGreen mt-8 mb-5"
+      >
+        Apply to be Rarement's artist
+      </Link>
+      <div className="flex flex-col items-center justify-center gap-6">
+        <ul className="flex items-center gap-5">
+          <li>
+            <a
+              href="/"
+              target="_blank"
+              rel="noopener noreferrer"
+              aria-label="Follow Rarement on Instagram"
+              className="w-[44px] h-[44px] flex items-center justify-center duration-200 border border-white/20 rounded-full bg-opaqueGray hover:text-[#F3F3F3]"
+            >
+              <Instagram />
+            </a>
           </li>
-        ))}
-      </ul>
-      <ul className="flex items-center gap-6 text-sm">
-        {InLinks.map((link, idx) => (
-          <li
-            key={idx}
-            className="cursor-pointer text-center duration-200 hover:text-[#F3F3F3]"
-          >
-            {link}
-          </li>
-        ))}
-      </ul>
+          {OutLinks.map((link, idx) => (
+            <li
+              key={idx}
+              className="w-[44px] h-[44px] flex items-center justify-center duration-200 border border-white/20 rounded-full bg-opaqueGray hover:text-[#F3F3F3]"
+            >
+              {link}
+            </li>
+          ))}
+        </ul>
+        <ul className="flex items-center gap-6 text-sm">
+          {InLinks.map((link, idx) => (
+            <li
+              key={idx}
+              className="cursor-pointer text-center duration-200 hover:text-[#F3F3F3]"
+            >
+              {link}
+            </li>
+          ))}
+        </ul>
+      </div>
     </footer>
   );
 }
