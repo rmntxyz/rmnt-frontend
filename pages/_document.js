bc53bbba39df44ee81ea47bfaf3008f3f3c6632f
doodoji <doodozik@gmail.com>
2023-05-05 06:47:27 +0900

    delete linebreak

--- pages/_document.js
+++ pages/_document.js
@@ -9,10 +9,7 @@ class MyDocument extends Document {
           <meta property="og:title" content="Rarement" />
           <meta property="og:description" content="Own the rare moment" />
           <meta property="og:url" content="https://nextjs.org/" />
-          <meta
-            property="og:image:url"
-            content="https://storage.googleapis.com/rmnt/thumbnail_RMNT_SYMBOL_85224726cb/thumbnail_RMNT_SYMBOL_85224726cb.png"
-          />
+          <meta property="og:image:url" content="https://storage.googleapis.com/rmnt/thumbnail_RMNT_SYMBOL_85224726cb/thumbnail_RMNT_SYMBOL_85224726cb.png" />
           <meta property="og:image:width" content="700" />
           <meta property="og:image:height" content="700" />
           <meta property="og:image:alt" content="Rarement" />
@@ -21,16 +18,10 @@ class MyDocument extends Document {
           <meta name="twitter:site" content="@rmntxyz" />
           <meta name="twitter:title" content="Rarement" />
           <meta name="twitter:description" content="Own the rare moment" />
-          <meta
-            name="twitter:image"
-            content="https://storage.googleapis.com/rmnt/thumbnail_RMNT_SYMBOL_85224726cb/thumbnail_RMNT_SYMBOL_85224726cb.png"
-          />
+          <meta name="twitter:image" content="https://storage.googleapis.com/rmnt/thumbnail_RMNT_SYMBOL_85224726cb/thumbnail_RMNT_SYMBOL_85224726cb.png" />
           <link rel="preconnect" href="https://fonts.gstatic.com" />
           <link rel="preconnect" href="https://fonts.googleapis.com" />
-          <link
-            rel="stylesheet"
-            href="https://fonts.googleapis.com/css2?family=Sen:wght@400;700;800&display=block"
-          />
+          <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Sen:wght@400;700;800&display=block" />
         </Head>
         <body>
           <Main />
